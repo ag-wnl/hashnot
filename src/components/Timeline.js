6ab3fba3cc61cb4c10ba151f23ce5094330b15a0
ag-wnl <agnideep22@gmail.com>
2023-09-22 16:02:50 +0530

    [UPD] Homepage UI init

---
+++ src/components/Timeline.js
@@ -0,0 +1,100 @@
+import * as React from 'react';
+import Timeline from '@mui/lab/Timeline';
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent, { timelineContentClasses } from '@mui/lab/TimelineContent';
+import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
+import TimelineDot from '@mui/lab/TimelineDot';
+import FastfoodIcon from '@mui/icons-material/Fastfood';
+import LaptopMacIcon from '@mui/icons-material/LaptopMac';
+import HotelIcon from '@mui/icons-material/Hotel';
+import RepeatIcon from '@mui/icons-material/Repeat';
+import Typography from '@mui/material/Typography';
+import PlayCircleFilledIcon from '@mui/icons-material/PlayCircleFilled';
+
+export default function CustomizedTimeline() {
+  return (
+    <Timeline sx={{
+        [`& .${timelineContentClasses.root}`]: {
+          flex: 0.2,
+        },
+      }}>
+      <TimelineItem>
+        <TimelineOppositeContent
+          sx={{ m: 'auto 0' }}
+          align="right"
+          variant="body2"
+          color="text.secondary"
+        >
+          9:30 am
+        </TimelineOppositeContent>
+        <TimelineSeparator>
+          <TimelineConnector />
+          <TimelineDot color="secondary">
+            <PlayCircleFilledIcon />
+          </TimelineDot>
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent sx={{ py: '12px', px: 2 }}>
+          <Typography style={{fontSize:'24px'}} variant="h6" component="span">
+            Start Exploring
+          </Typography >
+          <Typography style={{fontSize:'14px', color:'#ababab'}}>Checkout amazing Hackathons and Projects to take part</Typography>
+        </TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent
+          sx={{ m: 'auto 0' }}
+          variant="body2"
+          color="text.secondary"
+        >
+          10:00 am
+        </TimelineOppositeContent>
+        <TimelineSeparator>
+          <TimelineConnector />
+          <TimelineDot color="primary">
+            <LaptopMacIcon />
+          </TimelineDot>
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent sx={{ py: '12px', px: 2 }}>
+          <Typography variant="h6" component="span">
+            Code
+          </Typography>
+          <Typography>Because it&apos;s awesome!</Typography>
+        </TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineConnector />
+          <TimelineDot color="primary" variant="outlined">
+            <HotelIcon />
+          </TimelineDot>
+          <TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
+        </TimelineSeparator>
+        <TimelineContent sx={{ py: '12px', px: 2 }}>
+          <Typography variant="h6" component="span">
+            Sleep
+          </Typography>
+          <Typography>Because you need rest</Typography>
+        </TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
+          <TimelineDot color="secondary">
+            <RepeatIcon />
+          </TimelineDot>
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent sx={{ py: '12px', px: 2 }}>
+          <Typography variant="h6" component="span">
+            Repeat
+          </Typography>
+          <Typography>Because this is the life you love!</Typography>
+        </TimelineContent>
+      </TimelineItem>
+    </Timeline>
+  );
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,7 @@ import Footer from "../components/Footer";
 import { useContext } from "react";
 import { AuthContext } from "../context/authContext";
 import { Navigate, useNavigate } from "react-router-dom";
+import CustomizedTimeline from "../components/Timeline";
 
 
 function Home() {
@@ -19,9 +20,9 @@ function Home() {
             <div>
                 <Navbar /> 
                 <div class = 'banner'>
-                    <span class = 'banner-main-text'>Find your dream team and discover talented individuals across the globe</span>
-                    <span class = 'banner-sub-text'>Create or join a team and create the future together, from anywhere</span>
-                    <span class = 'banner-sub-text'>and with anyone</span>
+                    <h2 class = 'banner-main-text'>Find your Dream Team and Create a Better Future</h2>
+                    <span class = 'banner-sub-text'>Find the perfect team to join and develop amazing things</span>
+                    <span class = 'banner-sub-text'>no boundaries when it comes to tech</span>
 
                     <button 
                     onClick={()=> navigate(`/register`)}
@@ -32,26 +33,26 @@ function Home() {
 
                 <div class = 'section-below-banner'>
                     <div class = 'sq-box'>
-                        <span><strong>Join a Team</strong></span>
+                        <span style={{fontSize:'20px'}}><strong>Start the Journey</strong></span>
                         <span class = 'banner-sub-text'>Discover teams who are looking for exactly someone like you and hack the planet!</span>
                         <span class = 'go-btn'>
-                            Let's Go!
+                            Let's Start!
                             <img src = {open_lnk}/>
                         </span>
                     </div>
 
                     <div class = 'sq-box'>
-                        <span><strong>Join a Team</strong></span>
-                        <span class = 'banner-sub-text'>Discover teams who are looking for exactly someone like you and hack the planet!</span>
+                        <span style={{fontSize:'20px'}}><strong>Find the Team</strong></span>
+                        <span class = 'banner-sub-text'>Filter out posts to see exactly what you want and send a request!</span>
                         <span class = 'go-btn'>
-                            Let's Go!
+                            Let's Explore!
                             <img src = {open_lnk}/>
                         </span>
                     </div>
 
                     <div class = 'sq-box'>
-                        <span><strong>Join a Team</strong></span>
-                        <span class = 'banner-sub-text'>Discover teams who are looking for exactly someone like you and hack the planet!</span>
+                        <span style={{fontSize:'20px'}}><strong>Become a Part</strong></span>
+                        <span class = 'banner-sub-text'>Send request to join a team and chat with the Original Poster!</span>
                         <span class = 'go-btn'>
                             Let's Go!
                             <img src = {open_lnk}/>
@@ -59,6 +60,8 @@ function Home() {
                     </div>
                     
                 </div>
+
+                <CustomizedTimeline />
 
                 <Footer />
                 
